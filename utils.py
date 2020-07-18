33571ef37204e0379fe734ed18fbef94c0eeaf8f
Metin Bicer <m.bicer19@imperial.ac.uk>
2020-07-18 18:43:50 +0100

    loss calculation in training changed
    If the output is tuple (in Inception), loss is calculated differetly. First calculate a real loss. Then add auxiliary losses (with 30%)

--- utils.py
+++ utils.py
@@ -11,7 +11,7 @@ Utility functions for loading data, visualization etc
 import os
 import torch
 from torchvision import datasets, transforms, models
-
+import numpy as np
 
 # loads the data 
 # inputs:
@@ -55,3 +55,112 @@ def dataLoader(model, batch_size=32):
     loaders = {key: torch.utils.data.DataLoader(data, batch_size, shuffle=True) for key, data in dataset.items()}
     
     return loaders
+
+'''
+train the given model
+
+inputs:
+     n_epochs       --> number of epochs
+     loaders        --> train and validation loaders (keys are train and valid)
+     optimizer
+     criterion      --> loss function
+     save_path      --> name or path of the model to be saved
+     valid_loss_min --> previous validation loss (if trained previously, default Inf)
+     use_cuda       --> bool for using GPU computing (default depends on its availability)
+outputs:
+    model --> trained model
+'''
+def train(n_epochs, loaders, model, optimizer, criterion, save_path, 
+          valid_loss_min=np.Inf, use_cuda=torch.cuda.is_available()):
+    
+    for epoch in range(1, n_epochs+1):
+        # initialize variables to monitor training and validation loss
+        train_loss = 0.0
+        valid_loss = 0.0
+        
+        # write the training loss 2 times in an epoch
+        every_batch = int(len(loaders['train'])/2)
+        
+        ###################
+        # train the model #
+        ###################
+        model.train()
+        for batch_idx, (data, target) in enumerate(loaders['train']):
+            # move to GPU
+            if use_cuda:
+                data, target = data.cuda(), target.cuda()
+                # in case model is on cpu, move it to gpu
+                model.cuda()
+            else:
+                # in case model is in gpu, move it to cpu (because use_cuda=False)
+                model.cpu()
+                
+            # clear optimizer
+            optimizer.zero_grad()
+            
+            # forward pass
+            output = model(data)
+            
+            # if inceptionV3 model, then the output is tuple
+            # calculate loss
+            if isinstance(output, tuple):
+                # real part of the loss
+                loss = criterion(output[0], target)
+                
+                # other auxiliary losses (30% of this loss is added to real loss)
+                for aux_out in output[1:]:
+                    loss += 0.3 * criterion(aux_out, target)
+            else:
+                loss = criterion(output, target)
+            
+            # backward pass
+            loss.backward()
+            
+            # optimizer step
+            optimizer.step()
+
+            # average training loss 
+            train_loss = train_loss + ((1 / (batch_idx + 1)) * (loss.data - train_loss))
+            
+            if (batch_idx+1) % every_batch == 0:
+                print('Epoch: {}/{} \tBatch: {}/{} \tTraining Loss: {:.6f}'.format(
+                    epoch,
+                    n_epochs,
+                    batch_idx+1,
+                    len(loaders['train']),
+                    train_loss
+                    ))
+                
+        ######################    
+        # validate the model #
+        ######################
+        model.eval()
+        for batch_idx, (data, target) in enumerate(loaders['valid']):
+            # move to GPU
+            if use_cuda:
+                data, target = data.cuda(), target.cuda()
+            ## update the average validation loss
+            output = model(data)
+            loss = criterion(output, target)
+            
+            valid_loss = valid_loss + ((1 / (batch_idx + 1)) * (loss.data - valid_loss))
+        # print training/validation statistics 
+        print('Epoch: {} \tTraining Loss: {:.6f} \tValidation Loss: {:.6f}'.format(
+            epoch, 
+            train_loss,
+            valid_loss
+            ))
+        
+        # save the model if validation loss has decreased
+        if valid_loss <= valid_loss_min:
+            print('Validation loss decreased ({:.6f} --> {:.6f}).  Saving model ...'.format(
+                                                                                            valid_loss_min,
+                                                                                            valid_loss))
+            torch.save(model.state_dict(), save_path)
+            valid_loss_min = valid_loss
+    
+        # print a line starting new epoc
+        print('----------------------------------------------------------')
+        
+    # return trained model
+    return model
