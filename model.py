f0e96c0d06797ca1bfbbcc903cf8ef58fd7d9fef
Metin Bicer <m.bicer19@imperial.ac.uk>
2020-07-20 17:59:15 +0100

    prepareModel function added
    This creates the final layer depending on the model type

--- model.py
+++ model.py
@@ -11,6 +11,44 @@ import torch
 
 
 '''
+prepare the model (pretrained) for training. Freeze the conv layers, add 
+fc layer for the number of classes in the problem
+
+inputs:
+    model           --> pretrained model (of any available in pytorch)
+    num_classes     --> number of classes for the problem
+    init_fun        --> initializer for new fc layer
+    freeze_convnet  --> if True, dont train the convnet, else training starts 
+                        from the pretrained weightings
+outputs:
+    model --> pretrained model modified for the problem
+'''
+def prepareModel(model, num_classes, init_fun=None, freeze_convnet=True, **kwargs):
+    
+    # freeze the parameters of pretrained model
+    if freeze_convnet:
+        for param in model.parameters():
+            param.requires_grad = False
+    
+    # model can have different fully connected layer names (fc, classifier)
+    
+    # try fc
+    if hasattr(model, 'fc'):
+        model = _lastLayer(model, 'fc', num_classes, init_fun)
+    # try classifier
+    elif hasattr(model, 'classifier'):
+        model = _lastLayer(model, 'classifier', num_classes, init_fun)
+    elif hasattr(model, kwargs['layer_name']):
+        model = _lastLayer(model, kwargs['layer_name'], num_classes, init_fun)
+    # raise exception
+    else:
+        raise AttributeError('Check the name of the last layer in the model. \n' +
+                             'Then call the function with the argument "layer_name=your_layer_name"')
+        
+    return model
+
+
+'''
 creates an fc layer. This function is used in another function after checking
 for the layer_name. Otherwise, it throws AttributeError
 
